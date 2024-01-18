e8e19aad37008847e5f0c601bbf9a78ca16a205a
TJCole03 <tjc6254@gmail.com>
2024-01-18 06:32:07 -0500

    Service.jsx, creating and debugging input fields.

--- accounting_app/src/Service.jsx
+++ accounting_app/src/Service.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
+// const SERVICES = ['Reiki (in-person)', 'Reiki (distance)', 'Tarot Reading', 'Mediumship', 'Reiki Attunement', 'Tarot Class', 'Event/Retreat Payment']
 
 function ServiceField() {
-    const [service, setService] = useState([""])
-    // const [price, setPrice] = useState([0])
+    // const [date, setDate] = useState({Date})
+    const [service, setService] = useState("")
+    const [price, setPrice] = useState([0])
   
   
     const handleAddInput = () => {
       setService(...service, [{ service: '' }])
-    //   setPrice(...price, [{price: ''}])
+      setPrice(...price, [{price: ''}])
     }
   
     const handleChange = (e, index) => {
-      let { service, value} = e.target
+      let { service } = e.target
       let onChangeValue = [service]
-      onChangeValue[index][service] = value
+      onChangeValue[index][service] = service
       setService(onChangeValue)
     }
     
@@ -32,32 +34,61 @@ function ServiceField() {
 
     return (
         <div className="container">
-            {service.map((item, index) => (
-                <div className="input_container" key={index}>
+            {/* {service.map(( index) => ( */}
+            <div className="input_container"
+            // key={index}
+            >
+                    <h3>Date: </h3>
                     <input
-                        name="firstName"
+                        name="Date"
                         type="text"
-                        value={item.firstName}
-                        onChange={(event) => handleChange(event, index)}
+                        id={Date} //check on whether that's the right way to go about it
+                        onChange={(event) => handleChange(event )}
                     />
+                <h3>Service: </h3>
                     <input
-                        name="lastName"
+                        name="service"
                         type="text"
-                        value={item.lastName}
-                        onChange={(event) => handleChange(event, index)}
+                        id={service}
+                        onChange={(event) => handleChange(event )}
+                    />
+                    <h3>Price: </h3>
+                    <input
+                        name="price"
+                        type="text"
+                        id={price}
+                        onChange={(event) => handleChange(event )}
                     />
                     {service.length > 1 && (
-                        <button onClick={() => handleDeleteInput(index)}>Delete</button>
+                        <button onClick={() => handleDeleteInput()}>Delete</button>
                     )}
-                    {index === service.length - 1 && (
+                    { service.length - 1 && (
                         <button onClick={() => handleAddInput()}>Add</button>
                     )}
                 </div>
-            ))}
+            {/* )) */}
+            {/* } */}
 
             <div className="body"> {JSON.stringify(service)} </div>
         </div>
     )
 }
 
+// I've got the three input fields rendered on the UI and am able to type in them but not without errors showing up. 
+// want to take all three new values and organize them into tables and lists 
+// want to have a page that shows updated income for the year
+
 export default ServiceField
+
+                    {/* <select
+                        id="service"
+                        value={service}
+                        onChange={(event) => {
+                            setService(event.target.value)
+                        }}
+                    >
+                        <option />
+                        {SERVICES.map((service) => {
+                            <option key={service}>{service}</option>
+                        })}
+                    </select> */}

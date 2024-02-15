15db40b14c8d210e85c78de880a5b817582acfa9
TJCole03 <tjc6254@gmail.com>
2024-02-15 06:13:26 -0500

    submit button and functionality being added back in. trouble with error: Failed to construct 'FormData': parameter 1 is not of type 'HTMLFormElement'

--- accounting_app/src/components/NewTest.jsx
+++ accounting_app/src/components/NewTest.jsx
@@ -14,6 +14,44 @@ function ServiceField() {
     price: "",
   });
 
+  const handleSubmit = (e) => {
+    // prevents browser from reloading data
+        e.preventDefault();
+    // read form data
+        const form = e.target; 
+        const formData = new FormData(form)
+
+    // you can pass formData as fetch body directly: 
+        // fetch('/', { method: form.method, body: formData })
+        
+    // OR you can work with it as a plain object
+    const formJSON = Object.fromEntries(formData.entries())
+        inputs.push(formJSON)
+        console.log(formJSON, 'formJSON')
+        transferValue()
+        // console.log(index)
+        // console.log('formJson', [formJSON])
+     
+
+  }
+  const transferValue = (formJSON) => {
+    // setInputs("This is data from Parent Component to the Child Component.");
+    if (inputs.date && inputs.service && inputs.price) {
+      console.log({ inputs })
+    } else {
+        console.log('still closer because you know this doesnt work')
+    }
+    setInputs({formJSON})
+    clearState()
+  }
+  
+  const clearState = () => {
+    // setDate('')
+    // setService('');
+    // setPrice(0);
+    setInputs({date: "", service: "", price: 0})
+};
+
   function handleDate(e) {
     setInputs({
       ...inputs,
@@ -36,19 +74,23 @@ function ServiceField() {
   };
 
   return (
-    <>
+  <>
+    <form method="post" onSubmit={handleSubmit}>
+
       <label>
         Date:
         <input
           type='Date'
           value={inputs.date}
-          onChange={handleDate} />
+          onChange={handleDate}
+          action={transferValue}/>
       </label>
       <label>
         Service:
         <select
           value={inputs.service}
           onChange={handleService}
+          action={transferValue}
         >
           <option />
             {SERVICES.map((service) => (
@@ -59,11 +101,33 @@ function ServiceField() {
       </label>
       <label>
         Price:
-        <input value={inputs.price} onChange={handlePrice} />
+        <input
+          type='number'
+          value={inputs.price}
+          onChange={handlePrice}
+          action={transferValue}/>
       </label>
-      <p>
+      {/* <p>
         {inputs.date} {inputs.service} {inputs.price}
-      </p>
+      </p> */}
+      <table>
+                <thead>
+                <tr>
+                    <th scope='col'>  Date  </th>
+                    <th scope='col'>  Service  </th>
+                    <th scope='col'>  Gross Earnings  </th>
+                {/* <th scope='col'>Spending</th> */}
+                </tr>
+                <tr>
+                    <th scope="row" >{inputs.date}  </th>  
+                    <td scope="row">{inputs.service} </td>
+                    <td scope="row">{inputs.price}</td>
+                {/* <td>[insert expenditures]</td> */}
+                </tr>
+                </thead>
+      </table>
+        <Button primary type='submit' onClick={handleSubmit}>Submit</Button>
+  </form> 
     </>
   );
 }
